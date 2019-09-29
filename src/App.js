0898c2745a232aaf3d04e073d2f1ca9213c12afe
Harley Salas <harley@harleysalas.com>
2019-09-29 11:14:03 +0300

    finished sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -33,11 +33,8 @@ class App extends React.Component {
                 ...snapShot.data()
               }
             },
-            () => {
-              console.log(this.state.currentUser);
-            }
+            () => {}
           );
-          console.log(this.state);
         });
       }
       this.setState({ currentUser: userAuth });
